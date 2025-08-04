4f6f2dcf842d04744282630e62f9de30f66b6eef
ishluxx <ishimweemmanuel033@gmail.com>
2025-08-04 20:23:05 +0200

    Refactor code structure for improved readability and maintainability

--- client/pages/landing/landing.tsx
+++ client/pages/landing/landing.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
+import Footer from './footer'
 
 export default function Landing() {
   return (
+    <>
     <div className='flex flex-col items-center justify-center min-h-screen p-4'>
         <h1 className='text-center mb-5 '>Landing Page</h1>
         <p>Welcome to the landing page of our application.</p>
@@ -18,5 +20,7 @@ export default function Landing() {
             </Link>
         </Button>
     </div>
+        <Footer/>
+    </>
   )
 }
